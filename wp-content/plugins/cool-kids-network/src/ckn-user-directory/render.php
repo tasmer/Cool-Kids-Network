bf6a5373050f1b0f228b3188e755bb5f9cd76597
Zainoudine Soulé <tasmer.b@hotmail.com>
2024-10-20 01:23:06 +0200

    User Story 3 : display directory view only user with the allowed roles

--- wp-content/plugins/cool-kids-network/src/ckn-user-directory/render.php
+++ wp-content/plugins/cool-kids-network/src/ckn-user-directory/render.php
@@ -2,13 +2,17 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
-?>
-<div <?php echo get_block_wrapper_attributes(); ?>>
-    <button id="view-users-btn">View Users</button>
-    <table id="user-list" style="display: block">
-        <thead>
-        </thead>
-        <tbody>
-        </tbody>
-    </table>
-</div>
+
+if ( current_user_can( 'cooler_kid' ) || current_user_can( 'coolest_kid' ) ):
+	?>
+    <div <?php echo get_block_wrapper_attributes(); ?>>
+        <button id="view-users-btn">View Users</button>
+        <table id="user-list" style="display: block">
+            <thead>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
+<?php
+endif;
